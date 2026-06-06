6850c1994291c1439011831f51b400d478f4f350
Juampi <juanpablopfeiffer@gmail.com>
2026-06-06 11:14:24 -0300

    Agregado de localstorage

--- funcionalides/index.js
+++ funcionalides/index.js
@@ -9,36 +9,74 @@ const totalCarniceria = document.getElementById("gastoCarniceria");
 const totalVerduleria = document.getElementById("gastoVerduleria");
 const totalSupermercado = document.getElementById("gastoSupermercado");
 const totalCasa = document.getElementById("gastoCasa");
+const valorgeneral = document.getElementById("totalgeneral");
 
 let gastoTotalCarniceria = 0;
 let gastoTotalVerduleria = 0;
 let gastoTotalSupermercado = 0;
 let gastoTotalCasa = 0;
 
-boton.addEventListener("click", () =>{    
-        if(monto.value > 0){
-            if(categoria.value == "carniceria"){
-                gastoTotalCarniceria = gasto (ulCarniceria,totalCarniceria,gastoTotalCarniceria);
-        }   else if(categoria.value == "verduleria"){
-            gastoTotalVerduleria = gasto(ulVerduleria,totalVerduleria,gastoTotalVerduleria);
-        } else if (categoria.value == "supermercado"){
-            gastoTotalSupermercado = gasto(ulSupermercado,totalSupermercado,gastoTotalSupermercado);
+let gastos = [];
+
+const gastosGuardados = localStorage.getItem("gastos");
+
+if (gastosGuardados !== null) {
+    gastos = JSON.parse(gastosGuardados);
+
+    gastos.forEach((gasto) => {
+        console.log(gasto.categoria + ": " + gasto.monto);
+    });
+}
+
+boton.addEventListener("click", () => {
+    if (monto.value < 0 || monto.value == "") {
+        alert("Por favor ingresa un numero valido");
+    }
+
+    if (monto.value > 0) {
+        var numeroConvertido = Number(monto.value);
+
+        if (categoria.value == "carniceria") {
+            gastoTotalCarniceria = gasto(ulCarniceria, totalCarniceria, gastoTotalCarniceria);
+            agregarGasto(numeroConvertido, categoria.value);
+        } else if (categoria.value == "verduleria") {
+            gastoTotalVerduleria = gasto(ulVerduleria, totalVerduleria, gastoTotalVerduleria);
+            agregarGasto(numeroConvertido, categoria.value);
+        } else if (categoria.value == "supermercado") {
+            gastoTotalSupermercado = gasto(ulSupermercado, totalSupermercado, gastoTotalSupermercado);
+            agregarGasto(numeroConvertido, categoria.value);
         } else {
-            gastoTotalCasa = gasto(ulCasa,totalCasa,gastoTotalCasa)
+            gastoTotalCasa = gasto(ulCasa, totalCasa, gastoTotalCasa);
+            agregarGasto(numeroConvertido, categoria.value);
         }
     }
-    })
 
-    function gasto(listaUl,totalCategoria,gastoTotalCategoria){
+    let valorgral = gastoTotalCarniceria + gastoTotalCasa + gastoTotalSupermercado + gastoTotalVerduleria;
+    valorgeneral.textContent = "La suma total es $: " + valorgral;
+});
 
-        const nuevoli = document.createElement("li");
-        
-        nuevoli.textContent = monto.value;
-        listaUl.appendChild(nuevoli);
+function agregarGasto(montoNuevo, categoriaNueva) {
+    const gasto = {
+        categoria: categoriaNueva,
+        monto: montoNuevo
+    };
 
-        gastoTotalCategoria += Number(monto.value);
-        totalCategoria.textContent  = "El gasto total es: $" + gastoTotalCategoria;
+    gastos.push(gasto);
+    localStorage.setItem("gastos", JSON.stringify(gastos));
 
-        return gastoTotalCategoria;
+    const mostrarGastos = JSON.parse(localStorage.getItem("gastos"));
+    console.log(mostrarGastos);
+}
 
-    }
+function gasto(listaUl, totalCategoria, gastoTotalCategoria) {
+    const nuevoli = document.createElement("li");
+
+    nuevoli.textContent = monto.value;
+    listaUl.appendChild(nuevoli);
+
+    gastoTotalCategoria += Number(monto.value);
+    totalCategoria.textContent = "El gasto total es: $" + gastoTotalCategoria;
+    monto.value = "";
+
+    return gastoTotalCategoria;
+}
